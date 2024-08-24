b600470cfd33d87f6e1895b4756daa24ccb7056c
Luiz Bernardi <luizbernardi37@gmail.com>
2024-08-23 21:07:12 -0300

    feat(fazendo a pagina que o professor falou)

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        redlight: '#E55959',
+        greenlight: '#7FCDCD',
+        graylight: '#E7E7E7',
+        graytext: '#D9D9D9',
+        redbackground: '#EED0D0',
+        orangetext: '#C25500',
+        tela1white: '#ffffff',
+        navgren: '#89C5CC',
+
+      }
+    }
+  },
+  plugins: [],
+}
+
